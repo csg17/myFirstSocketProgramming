56479060a2d53441fa6ebf9b787ddbb58098fcda
csg17 <47957052+csg17@users.noreply.github.com>
2019-11-06 12:34:45 +0900

    second commit

---
+++ ftp-client.c
@@ -0,0 +1,96 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <string.h> 
+
+int 
+main (int argc, char const *argv[]) 
+{ 
+	struct sockaddr_in serv_addr; 
+	int sock_fd ;
+	int s, len ;
+	char buffer[1024] = {0}; 
+	char * data ;
+
+	char * cmdline ; // 'list' or 'get'
+	char * filename ;
+
+	if (argc == 4 && strcmp(argv[3], "list") == 0) {
+		cmdline = strdup("#list") ;
+	}
+	else if (argc == 5 && strcmp(argv[3], "get") == 0) {
+		cmdline = strdup(argv[4]) ; //파일 명을 보내주네요..
+	}
+	else {
+		//argv[1]: IP
+		//argv[2]: port
+		//argv[3]: command = {list, get}
+		//argv[4]: filename
+		fprintf(stderr, "Wrong number of arguments\n") ;
+		exit(EXIT_FAILURE) ;
+	}
+	
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+	if (sock_fd <= 0) {
+		perror("socket failed : ") ;
+		exit(EXIT_FAILURE) ;
+	} 
+
+	memset(&serv_addr, '0', sizeof(serv_addr)); 
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(atoi(argv[2])); 
+	if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0) {
+		perror("inet_pton failed : ") ; 
+		exit(EXIT_FAILURE) ;
+	} 
+
+	if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		perror("connect failed : ") ;
+		exit(EXIT_FAILURE) ;
+	}
+	// 연결됨!!!!! 서버랑 연결됨!!!!!1 
+	
+	data = cmdline ;
+	len = strlen(cmdline) ;
+	s = 0 ;
+	while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+		data += s ;
+		len -= s ;
+	}
+	shutdown(sock_fd, SHUT_WR) ;
+
+    // 그럼 이제 받아온걸 읽어주거나 파일 만들어서 저장.
+	if (strcmp(cmdline, "#list") == 0) {
+		char buf[1024] ;
+		//dat//a = 0x0 ;
+		//len = 0 ;
+		while ( (s = recv(sock_fd, buf, 1023, 0)) > 0 ) {
+			buf[s] = 0x0 ;
+			printf("%s", buf) ;
+		}
+		printf("\n") ;
+	}
+	else /* get file */ {
+        FILE *fp2;
+        char* buffer2;
+        int size = 2048;
+        int recv_file;
+
+        fp2 = fopen("recvieved.txt", "a");
+
+        if(fp2==NULL){
+            printf("File cannot be opened.ㅜㅜ\n");
+        }
+        bzero(buffer2, 2048);
+        while((recv_file = recv(sock_fd, buffer2, 2048, 0))>0){
+                int check_write = fwrite(/*stream: 정보 받아올 원천*/buffer2, sizeof(char), recv_file, fp2);
+                if(check_write < recv_file) {
+                    printf("Error ; file write\n");
+                }
+                bzero(buffer2, size);
+        }
+        printf("We ended receiving the file and creating the file.\n");
+	}
+} 
---
+++ ftp-server.c
@@ -0,0 +1,161 @@
+// Partly taken from https://www.geeksforgeeks.org/socket-programming-cc/
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+
+#include <string.h> 
+#include <pthread.h>
+#include <dirent.h>
+
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <netinet/in.h> 
+
+void *worker (void * arg)
+{
+	int conn ;
+	char buf[1024] ;
+
+	char * data = 0x0, * orig = 0x0 ;
+	int len = 0 ;
+	int s ;
+
+	conn = *((int *)arg) ; // 이 정수가 가리키는 곳 의 정수를 conn에 넣는다. 
+	free(arg) ;
+
+	while ( (s = recv(conn, buf, 1023, 0)) > 0 ) {
+		buf[s] = 0x0 ;
+		if (data == 0x0) {
+			data = strdup(buf) ;
+			len = s ;
+		}
+		else {
+			data = realloc(data, len + s + 1) ;
+			strncpy(data + len, buf, s) ;
+			data[len + s] = 0x0 ;
+			len += s ;
+		}
+	}
+	//받은게 data에 저장이 되죠.
+	printf(">%s\n", data) ;
+
+	//받은 명령이 list면
+	if (strcmp(data, "#list") == 0) {
+		DIR * d ;
+		d = opendir(".") ;
+		if (d == 0x0) {
+			perror("fail") ;
+			exit(EXIT_FAILURE) ;
+		}
+
+		struct dirent * e ;
+		//readdir == opendir
+		for (e = readdir(d) ; e != 0x0 ; e = readdir(d)) {
+			if (e->d_type == DT_REG) {
+				//printf("%s\n", e->d_name) ;
+				char * orig ;
+				data = malloc(sizeof(char) * (strlen(e->d_name) + 2)) ;
+				strcpy(data, e->d_name) ;
+				strcat(data, "\n") ;
+				len = strlen(data) ;
+				orig = data ;
+				printf("> %s", data) ;
+				while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
+					data += s ;
+					len -= s ;
+				}
+				free(orig) ;
+			}
+		}
+	}
+	// 받은 명령이 겟이면, data에 보내야 할 파일이름 있음. 
+	else if(strcmp(data, "get")==0)/* get file */ {
+		FILE *fp;
+        char buffer[1024];
+        fp = fopen(data, "rb");
+
+        if(fp == NULL){
+            printf("File not found\n");
+            //return 1;
+            return 0x0;
+        }
+        else{
+            printf("Found file!!\n");
+
+            //////read data into buffer
+            bzero(buffer, 1024);
+            int R;
+            while((R = fread(buffer, sizeof(char), sizeof(buffer), fp)>0)){
+                if(send(/*thread생성 시 파라미터로 new_socket보내줌 그거 conn으로 받음*/conn, buffer, R, 0)){
+                    printf("ERROR\n");
+                    exit(1);
+                }
+                bzero(buffer, 1024);
+            }
+            // 확인 ㄱ
+            printf("Ok sent to client!! :)\n");
+            close(conn);
+            printf("[Server] Connection with Client closed. \n");
+        }
+
+
+	}
+
+	shutdown(conn, SHUT_WR) ;
+	return 0x0 ;
+}
+
+int main (int argc, char const *argv[]) 
+{ 
+	int listen_fd, new_socket ; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+	
+	//if (argc != 2) {
+	//	fprintf(stderr, "Wrong number of arguments") ;
+	//	exit(EXIT_FAILURE) ;
+    //}
+
+	char buffer[1024] = {0}; 
+
+    printf("I'm server.\n");
+	listen_fd = socket(AF_INET /*IPv4*/, SOCK_STREAM /*TCP*/, 0 /*IP*/) ;
+	if (listen_fd == 0)  {
+		perror("socket failed : "); 
+		exit(EXIT_FAILURE); 
+	}
+	
+	memset(&address, '0', sizeof(address)); 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ; 
+	address.sin_port = htons(atoi(argv[1])); 
+
+	if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed : "); 
+		exit(EXIT_FAILURE); 
+	} 
+
+    printf("[SERVER IN]\n");
+	while (1) {
+		if (listen(listen_fd, 16 /* the size of waiting queue*/) < 0) { 
+			perror("listen failed : "); 
+			exit(EXIT_FAILURE); 
+		} 
+
+		new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen) ;
+		if (new_socket < 0) {
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		} 
+
+        // accept해서 받아온 값을 new_socket에 넣어준다. 
+		pthread_t worker_tid ;
+		int * worker_arg = (int *) malloc(sizeof(int)) ;
+		*worker_arg = new_socket ;
+
+		pthread_create(&worker_tid, 0x0, worker, (void *) worker_arg) ;
+	}
+} 
+
